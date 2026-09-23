8d3790c5f238862157f807970caf5a7cbd27de22
Ubuntu995 <za00011@163.com>
2026-09-23 09:05:47 +0800

    rust

--- advanced/learn_oo/target/debug/deps/learn_oo-a1a510720ce79af4.d
+++
@@ -1,5 +0,0 @@
-/home/weng/rust_learn/advanced/learn_oo/target/debug/deps/learn_oo-a1a510720ce79af4.d: src/main.rs
-
-/home/weng/rust_learn/advanced/learn_oo/target/debug/deps/learn_oo-a1a510720ce79af4: src/main.rs
-
-src/main.rs:
--- advanced/learn_oo/target/debug/learn_oo.d
+++
@@ -1,1 +0,0 @@
-/home/weng/rust_learn/advanced/learn_oo/target/debug/learn_oo: /home/weng/rust_learn/advanced/learn_oo/src/main.rs
